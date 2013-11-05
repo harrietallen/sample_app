1d12331222bf3b6eee224a663181e16e48de311f
Harriet Allen <hallen@madisoncollege.edu>
2013-11-04 20:24:30 -0600

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
    # before_save { self.email = email.downcase }
+     before_save { email.downcase! }
+     before_create :create_remember_token
+     has_secure_password
    
-    has_secure_password
-    before_save { email.downcase! }
    
 
     validates :name,  presence: true, length: { maximum: 50 }
@@ -16,17 +17,18 @@ class User < ActiveRecord::Base
       
       
     
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
 
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
 
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
+

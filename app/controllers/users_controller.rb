53101a4f323c3513d5dacbd975b50a875f52caa9
Harriet Allen <hallen@madisoncollege.edu>
2013-11-25 21:23:33 -0600

    Chapter 10 partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  def show
+def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
     def new
@@ -59,12 +60,7 @@ end
     
         # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+ 
 
     def correct_user
         @user = User.find(params[:id]) # doing here not in edit or update 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
+    has_many :microposts, dependent: :destroy
+
    # before_save { self.email = email.downcase }
      before_save { email.downcase! }
      before_create :create_remember_token
      has_secure_password
-   
-   
 
     validates :name,  presence: true, length: { maximum: 50 }
 
